2246af6d8181d784918fc3427bdc0373d72be103
Matheus Galvao <matheusrigonigalvao@gmail.com>
2022-10-14 20:55:38 +0100

    Events feed

--- party_seeker/lib/pages/events/EventCard.dart
+++ party_seeker/lib/pages/events/EventCard.dart
@@ -5,6 +5,48 @@ class EventCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: 250,
+      decoration: BoxDecoration(
+          border: Border.all(
+            width: 3,
+            color: Colors.white,
+          ),
+          borderRadius: const BorderRadius.all(Radius.circular(30))),
+      child: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Quiz Party',
+                  style: Theme.of(context).textTheme.headline1,
+                ),
+                Container(
+                  height: 50,
+                  width: 90,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(100),
+                    ),
+                  ),
+                  child: Center(
+                    child: Text(
+                      '1' + '€',
+                      style: Theme.of(context).textTheme.headline2?.copyWith(
+                            color: Colors.black,
+                          ),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- party_seeker/lib/pages/events/events.page.dart
+++ party_seeker/lib/pages/events/events.page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:party_seeker/pages/events/EventCard.dart';
 
 class EventsPage extends StatefulWidget {
   const EventsPage({super.key});
@@ -11,23 +13,49 @@ class _EventsPageState extends State<EventsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          onPressed: () {},
+          splashColor: Colors.transparent,
+          icon: const Icon(
+            Icons.add,
+            color: Colors.white,
+            size: 30,
+          ),
+        ),
+        title: Text(
+          "Next Events",
+          style: Theme.of(context).textTheme.headline1,
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            splashColor: Colors.transparent,
+            icon: const Icon(
+              CupertinoIcons.profile_circled,
+              color: Colors.white,
+              size: 30,
+            ),
+          ),
+        ],
+      ),
+      body: ListView.separated(
+        physics: const BouncingScrollPhysics(),
         padding: const EdgeInsets.only(
-          top: 60,
+          top: 20,
           bottom: 30,
-          left: 15,
-          right: 15,
+          left: 20,
+          right: 20,
         ),
-        child: Column(
-          children: [
-            Center(
-              child: Text(
-                "Next Events",
-                style: Theme.of(context).textTheme.headline1,
-              ),
-            )
-          ],
-        ),
+        separatorBuilder: (BuildContext context, int index) {
+          return const SizedBox(
+            height: 15,
+          );
+        },
+        itemCount: 10,
+        itemBuilder: (_, i) => EventCard(),
       ),
     );
   }
