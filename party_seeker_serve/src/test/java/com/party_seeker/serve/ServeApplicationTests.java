6c5d02fc4c6cd83a868824f8fc00e09a06216774
carlossouzal <carloslima68.cs@gmail.com>
2022-10-24 11:18:54 +0100

    create spring project

---
+++ party_seeker_serve/src/test/java/com/party_seeker/serve/ServeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.party_seeker.serve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
