0bb117b4cae035ed9d07304e64d4e633a853df07
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2022-12-14 12:24:50 +0000

    use secure storage to save the token

--- party_seeker/lib/httpRequest/user.api.dart
+++ party_seeker/lib/httpRequest/user.api.dart
@@ -1,10 +1,11 @@
+import 'package:flutter/foundation.dart';
 import 'package:party_seeker/models/user.dart';
 
 import 'implementation/dio_impl.dart';
 
 class UserApi {
   final DioImpl httpRequest = DioImpl();
-  final String baseUrl = "https://localhost:8080/users";
+  final String baseUrl = "http://localhost:8080";
 
   Future<User> createUser(User user) async {
     try {
@@ -15,8 +16,30 @@ class UserApi {
         throw "Error to create user";
       }
     } catch (ex) {
-      print(ex);
+      if (kDebugMode) {
+        print(ex);
+      }
       throw "Error to create user";
+    }
+  }
+
+  //TODO this endpoint needs to be changed, because its necessary to return all user
+  //information, but at moment returns only jwt token
+  Future<String> loginUser(String username, String password) async {
+    try {
+      var url = "$baseUrl/auth/signin";
+      var response = await httpRequest
+          .post(url, body: {"username": username, "password": password});
+
+      if (response.statusCode == 200) {
+        return response.data;
+      }
+      throw "Error to login, try again";
+    } catch (ex) {
+      if (kDebugMode) {
+        print(ex);
+      }
+      throw "Error to login, try again";
     }
   }
 }
--- party_seeker/lib/usecases/user.usecase.dart
+++ party_seeker/lib/usecases/user.usecase.dart
@@ -3,6 +3,7 @@ import 'package:party_seeker/models/result.dart';
 import 'package:party_seeker/models/user.dart';
 
 import '../httpRequest/user.api.dart';
+import '../shared_prefs/secure_storage.dart';
 
 class UserUsecase {
   late UserApi _userApi;
@@ -22,14 +23,15 @@ class UserUsecase {
   //   }
   // }
 
-  // Future<Result<User>> logion(String username, String password) async {
-  //   try {
-  //     var userCreated = await _userApi.login(username, password);
-  //     var globalController = GlobalController();
-  //     globalController.user = userCreated;
-  //     return Result(data: userCreated, isSuccess: true);
-  //   } catch (ex) {
-  //     return (Result(data: null, isSuccess: false));
-  //   }
-  // }
+  Future<Result<bool>> login(String username, String password) async {
+    try {
+      //TODO Change to get user and save on global storage
+      var token = await _userApi.loginUser(username, password);
+      var secureStorage = SecureStorage();
+      secureStorage.saveToken(token);
+      return Result(data: true, isSuccess: true);
+    } catch (ex) {
+      return (Result(data: null, isSuccess: false));
+    }
+  }
 }
